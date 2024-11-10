f57bcfe7066dc3c97371f9d1ce40fda0b6c98a86
hkattt <hugo.kat3@gmail.com>
2024-11-10 11:54:39 +1100

    Dinosaur jumping

---
+++ game-off-2024/scripts/dinosaur.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const GRAVITY: int = 300.0
+const JUMP_VELOCITY: int = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	move_and_slide()
