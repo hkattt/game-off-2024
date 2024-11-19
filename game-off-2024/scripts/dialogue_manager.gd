fc004e337824835cd5dab2b3d1589e882ab51245
hkattt <hugo.kat3@gmail.com>
2024-11-20 10:00:02 +1100

    Implemented dialogue manager

---
+++ game-off-2024/scripts/dialogue_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var dialogues: Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	load_dialogues("res://assets/dialogue.json")
+	
+func load_dialogues(file_path: String) -> void:
+	if FileAccess.file_exists(file_path):
+		var json_file: FileAccess = FileAccess.open(file_path, FileAccess.READ)
+		var json_as_text: String = FileAccess.get_file_as_string(file_path)
+		var json_as_dict: Dictionary = JSON.parse_string(json_as_text)
+		dialogues = json_as_dict["dialogues"]
+		print(dialogues)
+	else:
+		print("File does not exist")
