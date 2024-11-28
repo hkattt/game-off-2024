7f7946bd0906e644fb0977662f69d77808d5d3a9
hkattt <hugo.kat3@gmail.com>
2024-11-28 14:41:36 +1100

    Implemented scientist minigame

--- game-off-2024/scripts/accusation.gd
+++ game-off-2024/scripts/accusation.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
-@onready var child_button  = $MarginContainer/VBoxContainer/HBoxContainer/ChildButton
-@onready var doctor_button = $MarginContainer/VBoxContainer/HBoxContainer/DoctorButton
-@onready var chef_button   = $MarginContainer/VBoxContainer/HBoxContainer/ChefButton
+@onready var child_button       = $MarginContainer/VBoxContainer/BottomHBoxContainer/ChildButton
+@onready var doctor_button      = $MarginContainer/VBoxContainer/BottomHBoxContainer/DoctorButton
+@onready var chef_button        = $MarginContainer/VBoxContainer/TopHBoxContainer/ChefButton
+@onready var scientist_button   = $MarginContainer/VBoxContainer/TopHBoxContainer/ScientistButton
 
 const victory_scene: PackedScene = preload("res://scenes/victory.tscn")
 const lose_scene: PackedScene    = preload("res://scenes/lose.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -22,4 +23,7 @@ func _on_doctor_button_button_up() -> void:
 	get_tree().change_scene_to_packed(lose_scene)
 
 func _on_chef_button_button_up() -> void:
+	get_tree().change_scene_to_packed(lose_scene)
+
+func _on_scientist_button_button_up() -> void:
 	get_tree().change_scene_to_packed(lose_scene)
