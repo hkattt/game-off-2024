a8022474a285fa06179e02ddff50f7a34e086797
hkattt <hugo.kat3@gmail.com>
2024-11-29 22:12:31 +1100

    Child minigame tutorial text

--- game-off-2024/scripts/minigames/child/child_minigame.gd
+++ game-off-2024/scripts/minigames/child/child_minigame.gd
@@ -2,6 +2,8 @@ extends "res://scripts/minigames/minigame.gd"
 
 # Player node
 @onready var player: Node2D = $Player
+# Tutorial text
+@onready var tutorial_text: Label = $TutorialText
 
 # Obstacle scene
 @onready var obstacle_scene: PackedScene = preload("res://scenes/minigames/child/rock.tscn")
@@ -17,14 +19,17 @@ var win_timer: float = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if level != Level.EASY:
+		tutorial_text.visible = false
 	set_spawn_rates()
 			
 	spawn_timer = randf_range(spawn_time_min, spawn_time_max)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	spawn_timer -= delta
-	win_timer -= delta
+	if !tutorial_text.visible:
+		spawn_timer -= delta
+		win_timer -= delta
 	
 	# If the spawn timer runs out, spawn an obstacle 
 	if spawn_timer <= 0 && win_timer != INF:
--- game-off-2024/scripts/minigames/child/player.gd
+++ game-off-2024/scripts/minigames/child/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody2D
 const GRAVITY: int = 1000
 const JUMP_VELOCITY: int = -300
 
-@onready var jump_sound: AudioStreamPlayer2D
+@onready var tutorial_text: Label
 
 func _ready() -> void:
-	jump_sound = get_parent().get_node("JumpSound")
+	tutorial_text = get_parent().get_node("TutorialText")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,6 +15,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		tutorial_text.visible = false
 		velocity.y = JUMP_VELOCITY
 		SoundManager.play_sound(SoundManager.Sound.JUMP, 10.0)
 
