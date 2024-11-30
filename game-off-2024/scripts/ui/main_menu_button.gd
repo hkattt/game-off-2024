29d9fc97e2907338095ad55487bc9c639bb7660a
hkattt <hugo.kat3@gmail.com>
2024-11-30 21:13:36 +1100

    Main menu button

---
+++ game-off-2024/scripts/ui/main_menu_button.gd
@@ -0,0 +1,8 @@
+extends Button
+
+const main_menu: PackedScene = preload("res://scenes/screens/start_menu.tscn")
+
+func _on_button_up() -> void:
+	await FadeWindow.fade_out()
+	get_tree().change_scene_to_packed(main_menu)
+	await FadeWindow.fade_in()
