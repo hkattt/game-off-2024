cc256882a5cd98abca547ffa97e8baeb6adaba80
hkattt <hugo.kat3@gmail.com>
2024-12-01 18:27:13 +1100

    Main menu button working

--- game-off-2024/scripts/screens/start_menu.gd
+++ game-off-2024/scripts/screens/start_menu.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const story_scene: PackedScene = preload("res://scenes/screens/story.tscn")
+const story_scene_path: String = "res://scenes/screens/story.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		SoundManager.play_sound(SoundManager.Sound.CLICK, 20.0)
 		await FadeWindow.fade_out()
-		get_tree().change_scene_to_packed(story_scene)
+		get_tree().change_scene_to_file(story_scene_path)
 		await FadeWindow.fade_in()
--- game-off-2024/scripts/ui/main_menu_button.gd
+++ game-off-2024/scripts/ui/main_menu_button.gd
@@ -1,9 +1,9 @@
 extends TextureButton
 
-const main_menu: PackedScene = preload("res://scenes/screens/start_menu.tscn")
+const main_menu_path: String = "res://scenes/screens/start_menu.tscn"
 
 func _on_button_up() -> void:
 	SoundManager.play_sound(SoundManager.Sound.CLICK, 20.0)
 	await FadeWindow.fade_out()
-	get_tree().change_scene_to_packed(main_menu)
+	get_tree().change_scene_to_file(main_menu_path)
 	await FadeWindow.fade_in()
